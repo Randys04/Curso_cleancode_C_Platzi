9292cdd09529d660f01957e9305dff3051b587cf
Randy Moya <stevenmoya02@gmail.com>
2023-08-30 11:26:17 -0600

    Se modifico el codigo para que cumpla con el principio DRY (Don't repeat yourself)

--- Program.cs
+++ Program.cs
@@ -52,11 +52,7 @@ namespace ToDo
             {
                 Console.WriteLine("Ingrese el número de la tarea a remover: ");
                 // Show current taks
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+                listTasks();
 
                 string taskSelected = Console.ReadLine();
                 // Remove one position
@@ -98,13 +94,18 @@ namespace ToDo
             } 
             else
             {
-                Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+                listTasks();
             }
+        }
+
+        private static void listTasks()
+        {
+            Console.WriteLine("----------------------------------------");
+            for (int i = 0; i < TaskList.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + TaskList[i]);
+            }
+            Console.WriteLine("----------------------------------------");
         }
     }
 
